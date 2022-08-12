1237714950e5a6f0328809e8944bd1ccd8b8571d
Sam Gaus <sam@gaus.co.uk>
2022-08-12 09:57:11 +0100

    Add some missing new files from last commit

---
+++ packages/greenbox-web/src/components/PathTattoos.tsx
@@ -0,0 +1,41 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import { TattooDef } from "greenbox-data";
+
+import Thing from "./Thing";
+
+type Props = {
+  tattoos: TattooDef[];
+  playerTattoos: number[];
+  maxTattooLevel: number[];
+};
+
+function levelToTitle(level: number, max: number) {
+  if (max === 1) {
+    return level === 1 ? "Have" : "Do not have";
+  }
+
+  return `${level < max ? "Partially have" : "Have"} (level ${level} / ${max})`;
+}
+
+export default function PathTattoos({ tattoos, playerTattoos, maxTattooLevel }: Props) {
+  return (
+    <SimpleGrid columns={6} spacing={1}>
+      {tattoos.map(({ name, image }, index) => {
+        const level = playerTattoos[index] || 0;
+        const max = maxTattooLevel[index];
+        const i = Array.isArray(image) ? image[level - 1] : image;
+        return (
+          <Thing
+            key={name}
+            type="tattoo"
+            name={name}
+            image={`otherimages/sigils/${i}.gif`}
+            sourceWidth={50}
+            title={levelToTitle(level, max)}
+            state={level === 0 ? null : level < max ? "partial" : "complete"}
+          />
+        );
+      })}
+    </SimpleGrid>
+  );
+}
