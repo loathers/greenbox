397d7927be4d8b69fdb897609e964c1025faad5d
AJ Coppa <aj.coppa@gmail.com>
2023-10-08 10:04:33 -0400

    Add Dental Drip outfit

--- packages/greenbox-data/data/tattoos.ts
+++ packages/greenbox-data/data/tattoos.ts
@@ -32,6 +32,7 @@ export default [
   { name: "Cursed Skeleton Pirate Costume", image: "skelepiratetat", outfit: 155 },
   { name: "Cursed Zombie Pirate Costume", image: "zompirtat", outfit: 39 },
   { name: "Dark Bro's Vestments", image: "necbrotat", outfit: 83 },
+  { name: "Dental Drip", image: "dentaltat", outfit: 165 },
   { name: "Dinsey's Exoskeleton", image: "wdbraintat", outfit: 123 },
   { name: "Dire Drifter Duds", image: "spohobotat", outfit: 45 },
   { name: "Dreadful Bugbear Suit", image: "dvotat2", outfit: 96 },
