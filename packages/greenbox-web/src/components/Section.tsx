124a609632c8ece1c218856605c6d4ac416a9942
Joe Kirchoff <joe.kirchoff@epicgames.com>
2024-08-10 12:48:40 -0700

    Add wiki link to sections

--- packages/greenbox-web/src/components/Section.tsx
+++ packages/greenbox-web/src/components/Section.tsx
@@ -11,9 +11,11 @@ import {
 import AlphaImage from "./AlphaImage";
 import Progress from "./Progress";
 import Spinner from "./Spinner";
+import WikiSearchLink from "./WikiSearchLink";
 
 interface Props extends React.PropsWithChildren {
   title: string;
+  wiki?: string;
   icon: string;
   loading?: boolean;
   values: React.ComponentProps<typeof Progress>["values"];
@@ -22,6 +24,7 @@ interface Props extends React.PropsWithChildren {
 
 export default function Section({
   title,
+  wiki,
   icon,
   loading = false,
   values,
@@ -30,18 +33,21 @@ export default function Section({
 }: Props) {
   return (
     <AccordionItem isDisabled={loading}>
-      <AccordionButton fontSize="3xl">
-        <Stack direction="row" flex="1" textAlign="left">
-          <AlphaImage src={icon} />
-          <Heading fontSize={["xl", null, "3xl"]} fontWeight="normal">
-            {title}
-          </Heading>
-        </Stack>
-        <Box alignSelf="stretch" flex="1 1">
-          <Progress values={values} max={max} />
-        </Box>
-        {loading ? <Spinner /> : <AccordionIcon />}
-      </AccordionButton>
+      <Stack direction="row">
+        <AccordionButton fontSize="3xl">
+          <Stack direction="row" flex="1" textAlign="left">
+            <AlphaImage src={icon} />
+            <Heading fontSize={["xl", null, "3xl"]} fontWeight="normal">
+              {title}
+            </Heading>
+          </Stack>
+          <Box alignSelf="stretch" flex="1 1">
+            <Progress values={values} max={max} />
+          </Box>
+          {loading ? <Spinner /> : <AccordionIcon />}
+        </AccordionButton>
+        <WikiSearchLink page={wiki ?? title} text='?' fontSize="small"></WikiSearchLink>
+      </Stack>
       <AccordionPanel>
         <Stack spacing={4}>{children}</Stack>
       </AccordionPanel>
