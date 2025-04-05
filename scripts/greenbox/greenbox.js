0477668bc5d135dacfa2d8a48a484a62e4acf7d7
Rinn <Kirchoff.Joseph.P@gmail.com>
2025-04-05 18:33:26 +0000

    Build: (c42da93) Merge pull request #109 from loathers/not-tradable
    demodulizer not untradable

--- scripts/greenbox/greenbox.js
+++ scripts/greenbox/greenbox.js
@@ -6837,9 +6837,6 @@ var specialItems = [
   // hand-carved bow
   2721,
   // hand-carved staff
-  // Going Postal
-  2848,
-  // Gnomitronic Hyperspatial Demodulizer
   // The Pretty Good Escape
   4962,
   // mysterious silver lapel pin
